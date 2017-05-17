333a461190b5f1caf0fd87b722298cfab09561f2
k-kissa2 <k-kissa2@IT-867.si.net>
2017-05-17 16:59:20 +0200

    pusho

--- core/src/main/java/org/everit/meetup/core/MeetupActivator.java
+++ core/src/main/java/org/everit/meetup/core/MeetupActivator.java
@@ -5,12 +5,12 @@ import org.osgi.framework.BundleContext;
 
 public class MeetupActivator implements BundleActivator {
 
-	public void start(BundleContext arg0) throws Exception {
-		System.out.println("////////////// Hello OSGi world");
+	public void start(BundleContext context) throws Exception {
+		System.out.println("////////////// Hello OSGi world again");
 	}
 
 	public void stop(BundleContext arg0) throws Exception {
-		System.out.println("///////////// Bye OSGi world");
+		System.out.println("///////////// Bye OSGi world again");
 	}
 
 }
